541da81c0793fa30321773cda03fbd6c3afa5462
양윤상 <yanggak12@naver.com>
2021-05-20 11:44:00 +0900

    Setting: tsc-watch

--- index.js
+++
@@ -1,5 +0,0 @@
-const sayHi = (name, age, gender) => {
-    return `Hello, ${name}. You are ${age} years old and You are a ${gender}.`;
-};
-console.log(sayHi("yoonsang", 24, "male"));
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,6 +0,0 @@
-
-const sayHi = (name:string,age:number,gender:string):string => {
-    return `Hello, ${name}. You are ${age} years old and You are a ${gender}.`
-}
-
-console.log(sayHi("yoonsang",24,"male"));
