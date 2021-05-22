c2f8a67621d8474cd2bc995f051c107e74fd430c
양윤상 <yanggak12@naver.com>
2021-05-22 18:34:10 +0900

    Creating Block #2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import * as CryptoJs from 'crypto-js';
+
 class Block {
     public index : number;
     public hash : string;
     public previoushash : string;
     public data : string;
     public timestamp : number;
+
+    static calculateBlockHash = 
+    (index:number,previoushash:string,timestamp:number,data:string):string => 
+    CryptoJs.SHA256(index+previoushash+timestamp+data).toString();
+
     constructor(
         index : number,
         hash : string,
@@ -19,10 +26,17 @@ class Block {
 
 }
 
+
 const genesisBlock : Block = new Block(0,"20202020","","hello",123456);
 
-let blockchain:[Block] = [genesisBlock];
+let blockchain:Block[] = [genesisBlock];
 
-console.log(blockchain)
+
+const getBlockChain = ():Block[] => blockchain;
+
+const getLatestBlock = ():Block => blockchain[blockchain.length-1];
+
+const getNewTimestamp = ():number => Math.round(new Date().getTime()/1000)
+
 
 export {};
